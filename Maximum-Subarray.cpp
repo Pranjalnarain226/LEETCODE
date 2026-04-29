219741671f05a1dd157ab37901f1a4941910f49d
Pranjalnarain226 <pranjaln329@gmail.com>
2026-04-29 19:25:28 +0530

    [53] [Time Beats: 100.00%] [Memory Beats: 14.96%] - LeetPush

--- Maximum-Subarray.cpp
+++ Maximum-Subarray.cpp
@@ -1,16 +1,21 @@
-1class Solution {
-2public:
-3    int maxSubArray(vector<int>& nums) {
-4        int currSum = 0, maxSum = INT_MIN;
-5
-6        for(int val : nums) {
-7            currSum += val;
-8            maxSum = max(currSum, maxSum);
+1#include <vector>
+2#include <climits>
+3using namespace std;
+4
+5class Solution {
+6public:
+7    int maxSubArray(vector<int>& nums) {
+8        int currSum = 0, maxSum = INT_MIN;
 9
-10            if(currSum < 0) {
-11                currSum = 0;
-12            }
-13        }
-14        return maxSum;
-15    }
-16};
+10        for (int val : nums) {
+11            currSum += val;
+12            maxSum = max(currSum, maxSum);
+13
+14            if (currSum < 0) {
+15                currSum = 0;
+16            }
+17        }
+18
+19        return maxSum;
+20    }
+21};
